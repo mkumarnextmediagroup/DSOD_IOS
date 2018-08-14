f33245caee868ebd0e801d058672d2dd837a7564
yangentao <yangentao@gmail.com>
2018-08-15 00:46:30 +0800

    1

---
+++ dentist/dentist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dentist
+//
+//  Created by yet on 2018/8/15.
+//  Copyright © 2018年 nextmedia.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

72829825e561767b50d335230ec8ac3c5885e0c8
yang_entao <yangentao@forsaven.com>
2018-08-17 05:12:57 +0800

    colors

---
+++ dentist/dentist/common/Colors.h
@@ -0,0 +1,21 @@
+//
+// Created by yet on 2018/8/16.
+// Copyright (c) 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface Colors : NSObject
+
+@property(class, nonatomic, readonly) UIColor *primary;
+@property(class, nonatomic, readonly) UIColor *secondary;
+@property(class, nonatomic, readonly) UIColor *strokes;
+@property(class, nonatomic, readonly) UIColor *success;
+@property(class, nonatomic, readonly) UIColor *error;
+@property(class, nonatomic, readonly) UIColor *alert;
+@property(class, nonatomic, readonly) UIColor *textMain;
+@property(class, nonatomic, readonly) UIColor *textAlternate;
+
+
+@end

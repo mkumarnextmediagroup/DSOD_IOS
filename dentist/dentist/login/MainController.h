57dcf1b434848d001d25a4f0bb58198ddd5255f9
yang_entao <yangentao@forsaven.com>
2018-08-24 17:28:44 +0800

    1

---
+++ dentist/dentist/login/MainController.h
@@ -0,0 +1,14 @@
+//
+// Created by yet on 2018/8/17.
+// Copyright (c) 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseController.h"
+
+
+@interface MainController : BaseController
+
+@property NSString *msg;
+
+@end

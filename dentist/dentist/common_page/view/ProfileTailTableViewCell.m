983847d970e8ed5326db71c0f45c6a7a6917c0aa
yang_entao <yangentao@forsaven.com>
2018-09-07 18:15:25 +0800

    profile

--- dentist/dentist/common_page/view/ProfileTailTableViewCell.m
+++ dentist/dentist/common_page/view/ProfileTailTableViewCell.m
@@ -10,77 +10,77 @@
 #import "Fonts.h"
 #import "Common.h"
 
-@interface ProfileTailTableViewCell(){
-    NSInteger  _lineType;
+@interface ProfileTailTableViewCell () {
+	NSInteger _lineType;
 }
 
 @end
+
 @implementation ProfileTailTableViewCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        _avatar = [UIImageView new];
-        _avatar.frame = CGRectMake(18, 13, 48, 48);
-        [self addSubview:_avatar];
-        
-        
-        _specialityTitle = [UILabel new];
-        _specialityTitle.frame = CGRectMake(78, 22, SCREENWIDTH - 78, 16);
-        _specialityTitle.font = [Fonts semiBold:14];
-        _specialityTitle.textColor = rgb255(0x4a,  0x4a,  0x4a);
-        [self addSubview:_specialityTitle];
-        
-        
-        _speciality = [UILabel new];
-        _speciality.frame = CGRectMake(78, 42, SCREENWIDTH - 78, 14);
-        _speciality.font = [Fonts regular:12];
-        _speciality.textColor = rgb255(0x4a,  0x4a,  0x4a);
-        [self addSubview:_speciality];
-        
-    }
-    return self;
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+	self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+	if (self) {
+		_avatar = [UIImageView new];
+		_avatar.frame = CGRectMake(18, 13, 48, 48);
+
+		[self.contentView addSubview:_avatar];
+
+
+		_specialityTitle = [UILabel new];
+		_specialityTitle.frame = CGRectMake(78, 22, SCREENWIDTH - 78, 16);
+		_specialityTitle.font = [Fonts semiBold:14];
+		[_specialityTitle textColorMain];
+		[self.contentView addSubview:_specialityTitle];
+
+
+		_speciality = [UILabel new];
+		_speciality.frame = CGRectMake(78, 42, SCREENWIDTH - 78, 14);
+		_speciality.font = [Fonts regular:12];
+		[_speciality textColorMain];
+		[self.contentView addSubview:_speciality];
+
+	}
+	return self;
 }
 
 - (void)awakeFromNib {
-    [super awakeFromNib];
-    // Initialization code
+	[super awakeFromNib];
+	// Initialization code
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
+	[super setSelected:selected animated:animated];
 
-    // Configure the view for the selected state
+	// Configure the view for the selected state
 }
 
-- (void)setData:(Profile *) profile{
+- (void)setData:(Profile *)profile {
 
-    _lineType = profile.lineType;
-    _specialityTitle.text=profile.specialityTitle;
-    _speciality.text=profile.speciality;
-    [_avatar sd_setImageWithURL:[NSURL URLWithString:profile.avatatUrl]
-               placeholderImage:[UIImage imageNamed:profile.avataName]];
-    
+	_lineType = profile.lineType;
+	_specialityTitle.text = profile.specialityTitle;
+	_speciality.text = profile.speciality;
+	[_avatar sd_setImageWithURL:[NSURL URLWithString:profile.avatatUrl]
+	           placeholderImage:[UIImage imageNamed:profile.avataName]];
+
 }
 
 
--(void)drawRect:(CGRect)rect{
-    
-    CGContextRef context =UIGraphicsGetCurrentContext();
-    CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
-    CGContextFillRect(context, rect);
-    
-    //seperate line
-    CGContextSetStrokeColorWithColor(context,rgb255(0xe9,0xed, 0xf1).CGColor);
-    if (_lineType==0) {
-        CGContextStrokeRect(context,CGRectMake(0, rect.size.height-0.5, rect.size.width,1));
-    }else{
-        CGContextStrokeRect(context,CGRectMake(79, rect.size.height-0.5, rect.size.width,1));
-    }
-    
-    
-    
+- (void)drawRect:(CGRect)rect {
+
+	CGContextRef context = UIGraphicsGetCurrentContext();
+	CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
+	CGContextFillRect(context, rect);
+
+	//seperate line
+	CGContextSetStrokeColorWithColor(context, rgb255(0xe9, 0xed, 0xf1).CGColor);
+	if (_lineType == 0) {
+		CGContextStrokeRect(context, CGRectMake(0, rect.size.height - 0.5, rect.size.width, 1));
+	} else {
+		CGContextStrokeRect(context, CGRectMake(79, rect.size.height - 0.5, rect.size.width, 1));
+	}
+
+
 }
 
 @end

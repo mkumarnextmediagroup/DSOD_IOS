57cc3dcbc10e585ac150a9dc70663479c9d14c92
yangentao <yangentao@forsaven.com>
2018-09-08 21:57:20 +0800

    scroll

---
+++ dentist/dentist/cms/TestScrollPage.h
@@ -0,0 +1,13 @@
+//
+// Created by entaoyang on 2018/9/8.
+// Copyright (c) 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ScrollPage.h"
+
+
+@interface TestScrollPage : ScrollPage
+
+
+@end

759fb49376c5aae826d7d9d953eaae15e5b77feb
yang_entao <yangentao@forsaven.com>
2018-09-19 17:17:13 +0800

    itemview

--- dentist/dentist/cms/TestScrollPage.m
+++ dentist/dentist/cms/TestScrollPage.m
@@ -5,51 +5,72 @@
 
 #import "TestScrollPage.h"
 #import "Common.h"
-#import "LayoutParam.h"
 
 
 @implementation TestScrollPage {
-	UILabel *lb;
 }
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	self.navigationItem.rightBarButtonItem = [self navBarText:@"Test" target:self action:@selector(clickTest:)];
+	_scrollView = [UIScrollView new];
+	[self.view addSubview:_scrollView];
+	MASConstraintMaker *sm = [[[_scrollView layoutMaker] leftParent:0] rightParent:0];
+	[sm topParent:0];
+	if (self.tabBarController == nil) {
+		[sm bottomParent:0];
+	} else {
+		[sm bottomParent:-TABLEBAR_HEIGHT];
+	}
+	[sm install];
 
-	lb = [UILabel new];
-	lb.backgroundColor = UIColor.blueColor;
-	lb.text = @"==========11111===========";
-	[self.contentView addSubview:lb];
-	lb.layoutParam.height = 60;
+
+	_contentView = [UIView new];
+	[_scrollView addSubview:_contentView];
+	MASConstraintMaker *m = [_contentView layoutMaker];
+	m.edges.equalTo(_scrollView);
+	m.width.equalTo(_scrollView);
+	m.height.equalTo(_scrollView).with.priority(200);
+	[m install];
+
+	_contentView.backgroundColor = UIColor.whiteColor;
 
 
-	[self layoutLinearVertical];
+	for (NSInteger i = 0; i < 10; ++i) {
+		UILabel *lb = [self.contentView addLabel];
+		lb.numberOfLines = 0;
+		lb.text = strBuild([@(i) description], @" 发大水发生的发生的发生的发撒地方萨德富士达富士达富士达发撒地方撒地方======");
+		lb.layoutParam.height = -1;
+	}
+
 }
 
 
-- (void)rebuild {
-	for (int i = 0; i < 10; ++i) {
-		UILabel *lb = [UILabel new];
-		lb.backgroundColor = rgb255(i * 10, 255 - i * 10, 0);
-		lb.text = [NSString stringWithFormat:@"%d", i];
-		[self.contentView addSubview:lb];
-		[[[[[[lb layoutMaker] leftParent:0] rightParent:0] topParent:100 * i] heightEq:100] install];
+- (void)layoutLinearVertical {
+	CGFloat top = 0;
+	UIView *lastView = nil;
+	for (UIView *v in self.contentView.subviews) {
+		if (!v.hidden) {
+			LayoutParam *p = v.layoutParam;
+			CGFloat y = top + p.marginTop;
+			[[[[[[v layoutRemaker] leftParent:p.marginLeft] rightParent:-p.marginRight] topParent:y] heightEq:p.height] install];
+			top = y + p.height;
+			lastView = v;
+		}
+	}
+	if (lastView != nil) {
+		[_contentView.layoutUpdate.bottom.greaterThanOrEqualTo(lastView) install];
 	}
 }
 
-
-- (void)clickTest:(id)sender {
-	NSLog(@"click ");
-	UILabel *lb2 = [UILabel new];
-	lb2.backgroundColor = UIColor.greenColor;
-	lb2.text = @"======333333333===============";
-//	[self.contentView addSubview:lb2];
-	[self.contentView insertSubview:lb2 aboveSubview:lb];
-	lb2.layoutParam.height = 500;
-	lb2.layoutParam.marginLeft = 0;
-
-	[self layoutLinearVertical];
-
+- (UIView *)addGrayLine:(CGFloat)marginLeft marginRight:(CGFloat)marginRight {
+	UIView *v = [UIView new];
+	v.layoutParam.height = 1;
+	v.layoutParam.marginLeft = marginLeft;
+	v.layoutParam.marginRight = marginRight;
+	[self.contentView addSubview:v];
+	v.backgroundColor = Colors.cellLineColor;
+	return v;
 }
+
 
 @end

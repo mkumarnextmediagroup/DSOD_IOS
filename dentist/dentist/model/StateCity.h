110f124e7c58a555186056163f3e004448f6acff
yang_entao <yangentao@forsaven.com>
2018-09-19 18:07:46 +0800

    proto

---
+++ dentist/dentist/model/StateCity.h
@@ -0,0 +1,15 @@
+//
+// Created by entaoyang on 2018/9/19.
+// Copyright (c) 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface StateCity : NSObject
+
+@property NSString *state;
+@property NSString *city;
+@property NSString *zipCode;
+
+@end

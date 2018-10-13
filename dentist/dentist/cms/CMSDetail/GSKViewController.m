12e53c4efcd505b6bcca0e395312dcb21e98daad
Jacksun <1372951831@qq.com>
2018-10-13 21:48:27 +0800

    GSK view

--- dentist/dentist/cms/CMSDetail/GSKViewController.m
+++ dentist/dentist/cms/CMSDetail/GSKViewController.m
@@ -42,19 +42,92 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+     
+    [self createNav];
     
-    UINavigationItem *item = [self navigationItem];
-    item.title = @"SPONSORED CONTENT";
-    item.leftBarButtonItem = [self navBarImage:@"back_arrow" target:self action:@selector(onBack:)];
-    
-    self.table.rowHeight = 145;
+    self.table.tableHeaderView = [self makeHeaderView];
+    self.table.rowHeight = UITableViewAutomaticDimension;
+    self.table.estimatedRowHeight = 145;
     
     NSArray *ls = [Proto listArticle];
     self.items = ls;
 }
 
+- (void)createNav
+{
+    UIView *topVi = [UIView new];
+    topVi.backgroundColor = Colors.bgNavBarColor;
+    [self.view addSubview:topVi];
+    [[[[[topVi.layoutMaker leftParent:0] rightParent:0] topParent:0] heightEq:NAVHEIGHT] install];
+    
+    UILabel *content = [topVi addLabel];
+    content.font = [Fonts semiBold:15];
+    content.textColor = [UIColor whiteColor];
+    content.text = @"SPONSORED CONTENT";
+    content.textAlignment = NSTextAlignmentCenter;
+    [[[[content.layoutMaker leftParent:(SCREENWIDTH - 200)/2] topParent:23+NAVHEIGHT_OFFSET] sizeEq:200 h:40] install];
+    
+    UIButton *dismissBtn = [topVi addButton];
+    [dismissBtn setImage:[UIImage imageNamed:@"back_arrow"] forState:UIControlStateNormal];
+    [dismissBtn addTarget:self action:@selector(onBack:) forControlEvents:UIControlEventTouchUpInside];
+    [[[[dismissBtn.layoutMaker leftParent:0] topParent:24+NAVHEIGHT_OFFSET] sizeEq:60 h:40] install];
+}
+
 - (void)onBack:(UIButton *)btn {
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (UIView *)makeHeaderView2 {
+    UIView *seg = [self makeSegPanel];
+    seg.frame = makeRect(0, 0, SCREENWIDTH, 51);
+    return seg;
+}
+
+- (UIView *)makeHeaderView {
+    UIView *panel = [UIView new];
+    panel.frame = makeRect(0, 0, SCREENWIDTH, 300);
+    UIImageView *iv = [panel addImageView];
+    [iv scaleFillAspect];
+    iv.imageName = @"GSKPic";
+    [[[[[[iv layoutMaker] leftParent:0] rightParent:0] topParent:0] heightEq:249] install];
+    
+    UIButton *closeAd = [panel addButton];
+    [closeAd setImage:[UIImage imageNamed:@"close-white"] forState:UIControlStateNormal];
+    [[[[closeAd.layoutMaker topParent:22] rightParent:-22] sizeEq:24 h:24] install];
+    [closeAd onClick:self action:@selector(clickCloseAd:)];
+    
+    UIView *seg = [self makeSegPanel];
+    [panel addSubview:seg];
+    [[[[[seg.layoutMaker leftParent:0] rightParent:0] below:iv offset:0] heightEq:51] install];
+    
+    return panel;
+}
+
+- (UIView *)makeSegPanel {
+    UISegmentedControl *seg = [[UISegmentedControl alloc] initWithItems:segItems];
+    UIImage *img = colorImage(makeSize(1, 1), rgba255(221, 221, 221, 100));
+    [seg setDividerImage:img forLeftSegmentState:UIControlStateNormal rightSegmentState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    
+    [seg setTitleTextAttributes:@{NSFontAttributeName: [Fonts semiBold:12], NSForegroundColorAttributeName: Colors.textMain} forState:UIControlStateNormal];
+    [seg setTitleTextAttributes:@{NSFontAttributeName: [Fonts semiBold:12], NSForegroundColorAttributeName: Colors.textMain} forState:UIControlStateSelected];
+    
+    //colorImage(makeSize(1, 1), rgba255(247, 247, 247, 255))
+    [seg setBackgroundImage:[UIImage imageNamed:@"seg-bg"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [seg setBackgroundImage:[UIImage imageNamed:@"seg-sel"] forState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
+    for (NSUInteger i = 0; i < segItems.count; ++i) {
+        [seg setWidth:90 forSegmentAtIndex:i];
+    }
+    seg.selectedSegmentIndex = 0;
+    
+    UIScrollView *sv = [UIScrollView new];
+    [sv addSubview:seg];
+    
+    sv.contentSize = makeSize(90 * segItems.count, 50);
+    sv.showsHorizontalScrollIndicator = NO;
+    sv.showsVerticalScrollIndicator = NO;
+    segView = seg;
+    [seg addTarget:self action:@selector(onSegValueChanged:) forControlEvents:UIControlEventValueChanged];
+    return sv;
 }
 
 - (void)onSegValueChanged:(id)sender {
@@ -63,13 +136,16 @@
     Log(@(n ), title);
 }
 
+- (void)clickCloseAd:(id)sender {
+    self.table.tableHeaderView = [self makeHeaderView2];
+}
+
 - (Class)viewClassOfItem:(NSObject *)item {
     return GSKItemView.class;
 }
 
 - (CGFloat)heightOfItem:(NSObject *)item {
-    //    return 430;
-    return UITableViewAutomaticDimension;
+    return 145;
 }
 
 - (void)onBindItem:(NSObject *)item view:(UIView *)view {

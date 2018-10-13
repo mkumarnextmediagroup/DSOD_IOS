8b98b127de27b05351b18d4e800413627bf07c6a
wennan <wennan@forsaven.com>
2018-10-13 22:55:05 +0800

    terms

---
+++ dentist/dentist/login/DSOWebViewController.h
@@ -0,0 +1,18 @@
+//
+//  DSOWebViewController.h
+//  dentist
+//
+//  Created by wennan on 2018/10/13.
+//  Copyright © 2018年 thenextmediagroup.com. All rights reserved.
+//
+
+
+#import "BaseController.h"
+
+
+@interface DSOWebViewController : BaseController
+
+@property (nonatomic,strong) NSString *webTitle;
+@property (nonatomic,strong) NSString *localHtmlName;
+
+@end

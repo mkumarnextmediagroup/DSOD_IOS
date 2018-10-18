02518e7df65856451ae46420e01e75e7fc0b57e0
yangentao <yangentao@forsaven.com>
2018-10-18 17:59:48 +0800

    update ui

--- dentist/dentist/cms/CmsForYouPage.m
+++ dentist/dentist/cms/CmsForYouPage.m
@@ -174,18 +174,36 @@
     //
     CGFloat segw;
     segw=SCREENWIDTH*2/7.0;
-    CGFloat leftsegpoint=n*segw+segw/2.0;
+//    CGFloat leftsegpoint=n*segw+segw/2.0;
+//    //left
+//    CGFloat leftspace=(leftsegpoint-SCREENWIDTH/2.0);
+//    if (leftspace>0) {
+//        //right
+//        CGFloat rightsegpoint=segscrollView.contentSize.width-leftsegpoint;
+//        CGFloat rightspace=(rightsegpoint-SCREENWIDTH/2.0);
+//        if (rightspace<=0) {
+//            CGFloat rightbottomoffset=segscrollView.contentSize.width-segscrollView.bounds.size.width;
+//            [segscrollView setContentOffset:CGPointMake(rightbottomoffset, 0) animated:YES];
+//        }else{
+//            //left
+//            [segscrollView setContentOffset:CGPointMake(leftspace, 0) animated:YES];
+//        }
+//    }else if (leftspace<=0) {
+//        [segscrollView setContentOffset:CGPointMake(0, 0) animated:YES];
+//    }
+    CGFloat leftsegpoint=n*segw;
     //left
     CGFloat leftspace=(leftsegpoint-SCREENWIDTH/2.0);
     if (leftspace>0) {
         //right
         CGFloat rightsegpoint=segscrollView.contentSize.width-leftsegpoint;
-        CGFloat rightspace=(rightsegpoint-SCREENWIDTH/2.0);
+        CGFloat rightspace=(rightsegpoint-segscrollView.frame.size.width);
         if (rightspace<=0) {
             CGFloat rightbottomoffset=segscrollView.contentSize.width-segscrollView.bounds.size.width;
             [segscrollView setContentOffset:CGPointMake(rightbottomoffset, 0) animated:YES];
         }else{
-            [segscrollView setContentOffset:CGPointMake(leftspace, 0) animated:YES];
+            //left
+            [segscrollView setContentOffset:CGPointMake(leftsegpoint, 0) animated:YES];
         }
     }else if (leftspace<=0) {
         [segscrollView setContentOffset:CGPointMake(0, 0) animated:YES];

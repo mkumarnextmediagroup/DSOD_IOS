7c145719a3b3fef9f11ef4f1d363ab9ba39972b7
fengzhenrong <fengzhenrong@forsaven.com>
2018-10-19 21:58:15 +0800

    update ui

--- dentist/dentist/cms/GSKItemView.m
+++ dentist/dentist/cms/GSKItemView.m
@@ -48,9 +48,9 @@
     [dateLabel textColorAlternate];
     [[[[[dateLabel.layoutMaker centerYParent:0] rightParent:-edge] heightEq:topheight] widthEq:74] install];
     
-    UILabel *lineLabel = [topView lineLabel];
-    lineLabel.backgroundColor = Colors.cellLineColor;
-    [[[lineLabel.layoutMaker sizeEq:SCREENWIDTH h:1] topParent:topheight-1] install];
+//    UILabel *lineLabel = [topView lineLabel];
+//    lineLabel.backgroundColor = Colors.cellLineColor;
+//    [[[lineLabel.layoutMaker sizeEq:SCREENWIDTH h:1] topParent:topheight-1] install];
     
     UIView *contentView = self.addView;
     contentView.backgroundColor = rgb255(255, 255, 255);

0568c0de7f476ebc266320d413e4a84f0e8e9e73
sunxingguo <sunxingguo@forsaven.com>
2018-10-25 18:21:14 +0800

    detail api prepare

---
+++ dentist/dentist/cms/CMSDetail/Model/CommentModel.m
@@ -0,0 +1,13 @@
+//
+//  CommentModel.m
+//  dentist
+//
+//  Created by Jacksun on 2018/10/25.
+//  Copyright © 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import "CommentModel.h"
+
+@implementation CommentModel
+
+@end

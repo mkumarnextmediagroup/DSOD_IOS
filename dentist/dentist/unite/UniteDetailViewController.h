9cac8ee6fc5c63b9c41281676dfc0cfda6c112da
Jacksun <>
2018-11-01 15:59:05 +0800

    detail unite

---
+++ dentist/dentist/unite/UniteDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  UniteDetailViewController.h
+//  dentist
+//
+//  Created by Jacksun on 2018/11/1.
+//  Copyright © 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UniteDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ dentist/dentist/unite/UniteDetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  UniteDetailViewController.m
+//  dentist
+//
+//  Created by Jacksun on 2018/11/1.
+//  Copyright © 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import "UniteDetailViewController.h"
+
+@interface UniteDetailViewController ()
+
+@end
+
+@implementation UniteDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

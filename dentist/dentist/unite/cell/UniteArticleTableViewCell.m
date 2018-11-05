9a07a6a62ad6ee5848cb7dfc7dd3ac47d0a0767f
fengzhenrong <fengzhenrong@forsaven.com>
2018-11-05 19:55:06 +0800

    article list

--- dentist/dentist/unite/cell/UniteArticleTableViewCell.m
+++ dentist/dentist/unite/cell/UniteArticleTableViewCell.m
@@ -36,14 +36,14 @@
     UILabel *headLabel = self.addLabel;
     headLabel.font = [Fonts regular:13];
     headLabel.text = @"Transform your thinking";
-    [[[[[headLabel.layoutMaker leftParent:edge] rightParent:edge] heightEq:20] topParent:edge] install];
+    [[[[[headLabel.layoutMaker leftParent:edge] rightParent:edge] heightEq:20] topParent:8] install];
     
     UILabel *subHeadLabel = self.addLabel;
     [subHeadLabel textColorMain];
     subHeadLabel.numberOfLines = 2;
-    subHeadLabel.text = @"Understanding the DSO practise model";
+    subHeadLabel.text = @"Understanding the DSO practise model...";
     subHeadLabel.font = [Fonts regular:13];
-    [[[[[subHeadLabel.layoutMaker leftParent:edge] rightParent:edge] heightEq:30] below:headLabel offset:18] install];
+    [[[[subHeadLabel.layoutMaker leftParent:edge] sizeEq:SCREENWIDTH - 187 - edge *2 h:45] below:headLabel offset:2] install];
 }
 
 @end

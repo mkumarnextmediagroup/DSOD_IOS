6fb54dd811098a82ffdc60f41dabdb6526521c1a
Su Ho V <hovansu.it@gmail.com>
2018-11-05 21:07:29 +0700

    EditPracticeAddressViewController unit test

--- dentist/dentist/profile/EditPracticeAddressViewController.h
+++ dentist/dentist/profile/EditPracticeAddressViewController.h
@@ -8,12 +8,28 @@
 
 #import <UIKit/UIKit.h>
 #import "ScrollPage.h"
+#import "TitleEditView.h"
+#import "TitleMsgArrowView.h"
+#import "StateCity.h"
 
 @class Address;
 
 @interface EditPracticeAddressViewController : ScrollPage
 
+@property(nullable) TitleEditView *addr1View;
+@property(nullable) TitleEditView *addr2View;
+@property(nullable) TitleEditView *zipView;
+@property(nullable) TitleEditView *cityView;
+@property(nullable) TitleMsgArrowView *stateView;
 @property(nullable) Address *address;
 @property void (^saveCallback)(Address *address);
+
+- (void)queryStateCityByZipCode:(NSString *)zip;
+- (void)handleStateCity:(StateCity *)sc;
+- (void)clickBack:(id)sender;
+- (void)clickCancel:(UIButton *)btn;
+- (void)clickSave:(UIButton *)btn;
+- (void)clickState:(id)sender;
+- (void) handleResult:(NSObject *) item ls:(NSArray *) ls;
 
 @end

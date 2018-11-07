59d605a08ea87a135097d43f1a38c07ca87dbdf2
fengzhenrong <345030038@qq.com>
2018-11-07 18:04:21 +0800

    down api

--- dentist/dentist/cms/CMSDetail/GSKViewController.m
+++ dentist/dentist/cms/CMSDetail/GSKViewController.m
@@ -303,17 +303,18 @@ CMSModel *selectModel;
 
 
 
--(void)articleMoreAction:(NSInteger)articleid
-{
-    selectActicleId=articleid;
-    NSLog(@"ArticleMoreAction=%@",@(articleid));
-    NSArray *imgArr = [NSArray arrayWithObjects:@"downLoadIcon",@"shareIcon", nil];
-    DenActionSheet *denSheet = [[DenActionSheet alloc] initWithDelegate:self title:nil cancelButton:nil imageArr:imgArr otherTitle:@"Download",@"Share", nil];
-    [denSheet show:self.view];
-}
+//-(void)articleMoreAction:(NSInteger)articleid
+//{
+//    selectActicleId=articleid;
+//    NSLog(@"ArticleMoreAction=%@",@(articleid));
+//    NSArray *imgArr = [NSArray arrayWithObjects:@"downLoadIcon",@"shareIcon", nil];
+//    DenActionSheet *denSheet = [[DenActionSheet alloc] initWithDelegate:self title:nil cancelButton:nil imageArr:imgArr otherTitle:@"Download",@"Share", nil];
+//    [denSheet show:self.view];
+//}
 
 -(void)GSkArticleMoreActionModel:(CMSModel *)model
 {
+    selectModel=model;
     NSLog(@"ArticleMoreAction=%@",model.id);
     NSArray *imgArr = [NSArray arrayWithObjects:@"downLoadIcon",@"shareIcon", nil];
     DenActionSheet *denSheet = [[DenActionSheet alloc] initWithDelegate:self title:nil cancelButton:nil imageArr:imgArr otherTitle:@"Download",@"Share", nil];

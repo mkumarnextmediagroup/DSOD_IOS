a53dc1a3c7a832648436e835e9cd8d27fdfa3e8c
fengzhenrong <345030038@qq.com>
2018-11-08 16:55:29 +0800

    update

--- dentist/dentist/common/Download/DentistDataBaseManager.m
+++ dentist/dentist/common/Download/DentistDataBaseManager.m
@@ -18,6 +18,7 @@
 #import "DiscussInfo.h"
 #import "Json.h"
 #import "Proto.h"
+#import "IdName.h"
 
 @interface DentistDataBaseManager ()
 
@@ -41,38 +42,61 @@
 - (instancetype)init
 {
     if (self = [super init]) {
-        [self creatCMSCachesTable];
+        [self CMSCachesTable];
     }
     
     return self;
 }
 
-// 创表
-- (void)creatCMSCachesTable
+-(NSString *)getTableFilePath
 {
     // 数据库文件路径
     NSString *path = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"CMSCaches.sqlite"];
+    return path;
+}
+
+// 创表
+- (void)CMSCachesTable
+{
     
     // 创建队列对象，内部会自动创建一个数据库, 并且自动打开
-    _dbQueue = [FMDatabaseQueue databaseQueueWithPath:path];
+    _dbQueue = [FMDatabaseQueue databaseQueueWithPath:[self getTableFilePath]];
     
     [_dbQueue inDatabase:^(FMDatabase *db) {
         // 创表
 //        BOOL result = [db executeUpdate:@"CREATE TABLE IF NOT EXISTS t_CMSCaches (id VARCHAR(100) PRIMARY KEY, email VARCHAR(100),title text,content text,authorId VARCHAR(100),contentTypeId VARCHAR(100),categoryId VARCHAR(100),sponsorId VARCHAR(100),authorPhotoUrl text,author blob,contentTypeName text,categoryName text,sponsorName text, createdate timestamp not null default CURRENT_TIMESTAMP"];
-        BOOL result = [db executeUpdate:@"CREATE TABLE IF NOT EXISTS t_CMSCaches (id VARCHAR(100) PRIMARY KEY,title text,featuredMediaId VARCHAR(100),contentTypeId VARCHAR(100),categoryId VARCHAR(100),contentTypeName VARCHAR(100),categoryName VARCHAR(100), jsontext text,downstatus INTEGER default 0, createdate timestamp not null default CURRENT_TIMESTAMP)"];
-        if (result) {
-        }else {
-            NSLog(@"缓存数据表创建失败");
-        }
+        [self creatCMSCachesTable:db];
+        [self createCMSContentCachesTable:db];
     }];
+}
+
+-(void)creatCMSCachesTable:(FMDatabase *)db
+{
+    BOOL result = [db executeUpdate:@"CREATE TABLE IF NOT EXISTS t_CMSCaches (id VARCHAR(100) PRIMARY KEY,title text,featuredMediaId VARCHAR(100),contentTypeId VARCHAR(100),categoryId VARCHAR(100),contentTypeName VARCHAR(100),categoryName VARCHAR(100), jsontext text,downstatus INTEGER default 0, createdate timestamp not null default CURRENT_TIMESTAMP)"];
+    if (result) {
+        NSLog(@"缓存t_CMSCaches数据表创建成功");
+    }else {
+        NSLog(@"缓存t_CMSCaches数据表创建失败");
+    }
+}
+
+-(void)createCMSContentCachesTable:(FMDatabase *)db {
+    BOOL result = [db executeUpdate:@"CREATE TABLE IF NOT EXISTS t_CMSContentCaches (id VARCHAR(100) PRIMARY KEY, jsontext text)"];
+    if (result) {
+        NSLog(@"缓存t_CMSContentCaches数据表创建成功");
+    }else {
+        NSLog(@"缓存t_CMSContentCaches数据表创建失败");
+    }
 }
 
 -(void)queryDetailCmsCaches:(NSString *)articleid completed:(void(^)(DetailModel *model))completed
 {
     [_dbQueue inDatabase:^(FMDatabase *db) {
+        [db open];
         NSString *jsontext = [db stringForQuery:@"SELECT jsontext FROM t_CMSCaches WHERE id = ?", articleid];
         DetailModel *detail = [[DetailModel alloc] initWithJson:jsontext];
         detail.discussInfos = [self commentConvertDiscussInfo:detail.comment];
+        [db close];
         if (completed) {
             completed(detail);
         }
@@ -102,6 +126,7 @@
 {
     NSInteger limit=20;
     [_dbQueue inDatabase:^(FMDatabase *db) {
+        [db open];
         FMResultSet *resultSet;
         resultSet = [db executeQuery:@"SELECT * FROM t_CMSCaches order by createdate desc limit ? offset ?",[NSNumber numberWithInteger:limit],[NSNumber numberWithInteger:skip]];
         NSMutableArray *tmpArr = [NSMutableArray array];
@@ -117,6 +142,7 @@
             model.downstatus=[NSString stringWithFormat:@"%@",@([resultSet intForColumn:@"downstatus"])];
             [tmpArr addObject:model];
         }
+        [db close];
         if (completed) {
             completed(tmpArr);
         }
@@ -127,6 +153,7 @@
 {
     NSInteger limit=20;
     [_dbQueue inDatabase:^(FMDatabase *db) {
+        [db open];
         FMResultSet *resultSet;
         if(![NSString isBlankString:categoryId] && ![NSString isBlankString:contentTypeId]){
             resultSet = [db executeQuery:@"SELECT * FROM t_CMSCaches where categoryId = ? and contentTypeId = ? order by createdate desc limit ? offset ?",categoryId,contentTypeId,[NSNumber numberWithInteger:limit],[NSNumber numberWithInteger:skip]];
@@ -151,6 +178,7 @@
             model.downstatus=[NSString stringWithFormat:@"%@",@([resultSet intForColumn:@"downstatus"])];
             [tmpArr addObject:model];
         }
+        [db close];
         if (completed) {
             completed(tmpArr);
         }
@@ -162,7 +190,9 @@
 {
     if (![NSString isBlankString:cmsmodel.id]) {
         [_dbQueue inDatabase:^(FMDatabase *db) {
+            [db open];
             if ([self CheckIsHasDown:db articleid:cmsmodel.id]) {
+                [db close];
                 if (completed) {
                     completed(YES);
                 }
@@ -177,6 +207,7 @@
                     urlstr = cmsmodel.featuredMedia[@"code"];
                 }
                 BOOL result = [db executeUpdate:@"INSERT INTO t_CMSCaches (id,title, featuredMediaId,contentTypeId,categoryId,contentTypeName,categoryName,downstatus) VALUES (?,?, ?, ?, ?, ?, ?,?)", cmsmodel.id,cmsmodel.title,urlstr,cmsmodel.contentTypeId,cmsmodel.categoryId,cmsmodel.contentTypeName,cmsmodel.categoryName,[NSNumber numberWithInteger:1]];
+                [db close];
                 if (completed) {
                     completed(result);
                 }
@@ -199,7 +230,9 @@
 {
     if (![NSString isBlankString:articleid] && ![NSString isBlankString:jsontext]) {
         [_dbQueue inDatabase:^(FMDatabase *db) {
+            [db open];
             BOOL result = [db executeUpdate:@"INSERT INTO t_CMSCaches (id, jsontext) VALUES (?, ?)", articleid,jsontext];
+            [db close];
             if (completed) {
                 completed(result);
             }
@@ -220,15 +253,18 @@
 {
     if (![NSString isBlankString:articleid] && ![NSString isBlankString:jsontext]) {
         [_dbQueue inDatabase:^(FMDatabase *db) {
+            [db close];
             BOOL result = [db executeUpdate:@"UPDATE t_CMSCaches set jsontext =?,downstatus=5 where id=? ", jsontext,articleid];
-            if (completed) {
-                completed(result);
-            }
             if (result) {
                 [self postStateChangeNotificationWithDatabase:db articleid:articleid status:5];
             }else {
                 NSLog(@"更新cms失败");
             }
+            [db close];
+            if (completed) {
+                completed(result);
+            }
+            
         }];
     }else{
         if (completed) {
@@ -252,7 +288,9 @@
 {
     if (model) {
         [_dbQueue inDatabase:^(FMDatabase *db) {
+            [db open];
             NSInteger status = [db intForQuery:@"SELECT 1 FROM t_CMSCaches WHERE id = ?", model.id];
+            [db close];
             if (completed) {
                 completed(status);
             }
@@ -288,7 +326,9 @@
 {
     if (![NSString isBlankString:articleid]) {
         [_dbQueue inDatabase:^(FMDatabase *db) {
+            [db close];
             BOOL result = [db executeUpdate:@"delete from t_CMSCaches where id= ? ",articleid];
+            [db close];
             if (completed) {
                 completed(result);
             }
@@ -305,5 +345,104 @@
     
 }
 
+#pragma mark -------缓存接口返回数据
+//MARK: 更新数据缓存记录
+-(void)updateContentCaches:(NSString *)key jsontext:(NSString *)jsontext completed:(void (^)(BOOL result))completed{
+    [_dbQueue inDatabase:^(FMDatabase *db) {
+        [db open];
+        NSInteger status = [db intForQuery:@"SELECT 1 FROM t_CMSContentCaches WHERE id = ?", key];
+        if (status==1) {
+            //存在该记录更新
+            BOOL result = [db executeUpdate:@"UPDATE t_CMSContentCaches set jsontext =? where id=? ", jsontext,key];
+            [db close];
+            if (completed) {
+                completed(result);
+            }
+        }else{
+           //添加
+            BOOL result = [db executeUpdate:@"INSERT INTO t_CMSContentCaches (id, jsontext) VALUES (?, ?)", key,jsontext];
+            [db close];
+            if (completed) {
+                completed(result);
+            }
+        }
+    }];
+}
+
+//MARK: 获取媒体列表缓存数据
+-(void)queryAllContentsCaches:(NSString *)key completed:(void (^)(NSArray<CMSModel *> *array))completed
+{
+    FMDatabaseQueue *newqueue=[FMDatabaseQueue databaseQueueWithPath:[self getTableFilePath]];
+    [newqueue inDatabase:^(FMDatabase *db) {
+        [db open];
+        NSString *jsontext = [db stringForQuery:@"SELECT jsontext FROM t_CMSContentCaches WHERE id = ?", key];
+        NSArray *arr = jsonParseObject(jsontext);
+        NSMutableArray *resultArray = [NSMutableArray array];
+        for (NSDictionary *d in arr) {
+            CMSModel *item = [[CMSModel alloc] initWithJson:jsonBuild(d)];
+            if (item) {
+                [resultArray addObject:item];
+            }
+        }
+        [db close];
+        if (completed) {
+            completed(resultArray);
+        }
+    }];
+}
+
+-(void)queryContentsCaches:(NSString *)key completed:(void (^)(NSArray *array))completed
+{
+    FMDatabaseQueue *newqueue=[FMDatabaseQueue databaseQueueWithPath:[self getTableFilePath]];
+    [newqueue inDatabase:^(FMDatabase *db) {
+        [db open];
+        NSString *jsontext = [db stringForQuery:@"SELECT jsontext FROM t_CMSContentCaches WHERE id = ?", key];
+        NSArray *arr = jsonParseObject(jsontext);
+        [db close];
+        if (completed) {
+            completed(arr);
+        }
+    }];
+}
+
+-(void)queryContentTypesCaches:(void(^)(NSArray<IdName *> *array))completed {
+    
+    FMDatabaseQueue *newqueue=[FMDatabaseQueue databaseQueueWithPath:[self getTableFilePath]];
+    [newqueue inDatabase:^(FMDatabase *db) {
+        [db open];
+        NSString *key=@"findAllContentType";
+        NSString *jsontext = [db stringForQuery:@"SELECT jsontext FROM t_CMSContentCaches WHERE id = ?", key];
+        NSArray *arr = jsonParseObject(jsontext);
+        NSMutableArray *resultArray = [NSMutableArray array];
+        for (NSDictionary *d in arr) {
+            IdName *item = [[IdName alloc] initWithJson:jsonBuild(d)];
+            [resultArray addObject:item];
+        }
+        [db close];
+        if (completed) {
+            completed(resultArray);
+        }
+    }];
+}
+
+-(void)queryCategoryTypesCaches:(void(^)(NSArray<IdName *> *array))completed {
+    
+    FMDatabaseQueue *newqueue=[FMDatabaseQueue databaseQueueWithPath:[self getTableFilePath]];
+    [newqueue inDatabase:^(FMDatabase *db) {
+        [db open];
+        NSString *key=@"findAllCategory";
+        NSString *jsontext = [db stringForQuery:@"SELECT jsontext FROM t_CMSContentCaches WHERE id = ?", key];
+        NSArray *arr = jsonParseObject(jsontext);
+        NSMutableArray *resultArray = [NSMutableArray array];
+        for (NSDictionary *d in arr) {
+            IdName *item = [[IdName alloc] initWithJson:jsonBuild(d)];
+            [resultArray addObject:item];
+        }
+        [db close];
+        if (completed) {
+            completed(resultArray);
+        }
+    }];
+}
 
 @end

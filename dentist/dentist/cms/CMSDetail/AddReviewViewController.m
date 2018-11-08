f6dd57283dd2f50430ddf20dcd31e641639a216f
yangentao <yangentao@forsaven.com>
2018-11-08 15:57:40 +0800

    add review fix bug

--- dentist/dentist/cms/CMSDetail/AddReviewViewController.m
+++ dentist/dentist/cms/CMSDetail/AddReviewViewController.m
@@ -37,12 +37,53 @@
 
     
     [self buildSubViews];
-    
+
     [self buildAuthor];
-    
+
     [self bulidTextVi];
     // Do any additional setup after loading the view.
+    
+    //注册观察键盘的变化
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(transformView:) name:UIKeyboardWillChangeFrameNotification object:nil];
+
 }
+
+
+- (void)dealloc{
+   [ [NSNotificationCenter defaultCenter]removeObserver:self ];
+}
+
+
+//键盘回收
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    for(UIView *view in self.view.subviews)
+    {
+        [view resignFirstResponder];
+    }
+}
+
+//移动UIView
+-(void)transformView:(NSNotification *)aNSNotification
+{
+    //获取键盘弹出前的Rect
+    NSValue *keyBoardBeginBounds=[[aNSNotification userInfo]objectForKey:UIKeyboardFrameBeginUserInfoKey];
+    CGRect beginRect=[keyBoardBeginBounds CGRectValue];
+    
+    //获取键盘弹出后的Rect
+    NSValue *keyBoardEndBounds=[[aNSNotification userInfo]objectForKey:UIKeyboardFrameEndUserInfoKey];
+    CGRect  endRect=[keyBoardEndBounds CGRectValue];
+    
+    //获取键盘位置变化前后纵坐标Y的变化值
+    CGFloat deltaY=endRect.origin.y-beginRect.origin.y;
+    NSLog(@"看看这个变化的Y值:%f",deltaY);
+    
+    //在0.25s内完成self.view的Frame的变化，等于是给self.view添加一个向上移动deltaY的动画
+    [UIView animateWithDuration:0.25f animations:^{
+        [self.view setFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y+deltaY+50, self.view.frame.size.width, self.view.frame.size.height)];
+    }];
+}
+
 
 - (void)onBack:(UIButton *)btn {
     [self.navigationController popViewControllerAnimated:YES];

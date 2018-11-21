72d1b21ccdda897aceed9aaa103ef800c4d4556c
Jacksun <>
2018-11-21 21:25:47 +0800

    article list

--- dentist/dentist/unite/cell/UniteArticleTableViewCell.h
+++ dentist/dentist/unite/cell/UniteArticleTableViewCell.h
@@ -13,9 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UniteArticleTableViewCell : UITableViewCell
 
-- (void)bindInfo:(NSArray *)infoArr;
-
-- (void)bindSearchInfo:(DetailModel *)infoModel;
+- (void)bindInfo:(DetailModel *)infoModel;
 
 @end
 

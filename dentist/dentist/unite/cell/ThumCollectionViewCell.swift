073f570cb4a8f94edcc116b42b50d1669c7b51b1
fengzhenrong <345030038@qq.com>
2018-11-22 18:51:36 +0800

    fix bugs

--- dentist/dentist/unite/cell/ThumCollectionViewCell.swift
+++ dentist/dentist/unite/cell/ThumCollectionViewCell.swift
@@ -35,11 +35,18 @@ class ThumCollectionViewCell: BasePageCollectionCell,UIWebViewDelegate {
     @IBOutlet weak var removeBookmarkButton: UIButton!
     @IBOutlet weak var frontImageVIew: UIImageView!
     
+    var titleView:UIView?
+    var webtitleLabel:UILabel?
+    var websubTitleLabel:UILabel?
+    var authorLabel:UILabel?
+    
     var selectIndexpath:IndexPath!
     //  2. 声明变量
     var delegate:ThumCollectionViewCellDelegate?
     
     var detailModel:DetailModel?
+    let edge:CGFloat = 18.0
+    var imageViewHeight:CGFloat?
     
     @IBOutlet weak var frontWebView: UIWebView!
     var mywebView:UIWebView?
@@ -47,6 +54,8 @@ class ThumCollectionViewCell: BasePageCollectionCell,UIWebViewDelegate {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        imageViewHeight =  (toScreenFrame().width * 2 / 3)
+        
         frontImageVIew.scaleFillAspect()
         frontImageVIew.clipsToBounds=true
         ArchiiveButton.layer.borderColor=UIColor.darkGray.cgColor
@@ -99,6 +108,33 @@ class ThumCollectionViewCell: BasePageCollectionCell,UIWebViewDelegate {
     
     @objc func setModelData(_ model:DetailModel){
         detailModel=model
+    }
+    
+    func setUI() -> Void {
+        
+        titleView=UIView()
+        self.addSubview(titleView!)
+        titleView?.layoutMaker.below(frontImageVIew, offset: 10)?.leftParent(edge)?.rightParent(-edge)?.install()
+        webtitleLabel=UILabel()
+        webtitleLabel?.font=Fonts.semiBold(10)
+        webtitleLabel?.textColor=rgbHex(0x0e78b9)
+        webtitleLabel?.numberOfLines=0
+        titleView?.addSubview(webtitleLabel!)
+        webtitleLabel?.layoutMaker.leftParent(0)?.rightParent(0)?.topParent(0)?.install()
+        websubTitleLabel=UILabel()
+        websubTitleLabel?.font=Fonts.semiBold(12)
+        websubTitleLabel?.textColor=UIColor.black
+        websubTitleLabel?.numberOfLines=0
+        titleView?.addSubview(websubTitleLabel!)
+        websubTitleLabel?.layoutMaker.leftParent(0)?.rightParent(0)?.below(titleLabel, offset: 0)?.install()
+        authorLabel=UILabel()
+        authorLabel?.font=Fonts.semiBold(8)
+        authorLabel?.textColor=rgbHex(0x9b9b9b)
+        authorLabel?.numberOfLines=0
+        titleView?.addSubview(authorLabel!)
+//        [[[[[authorLabel.layoutMaker leftParent:0] rightParent:0] below:subTitleLabel offset:0] bottomParent:0]
+        authorLabel?.layoutMaker.leftParent(0)?.rightParent(0)?.below(websubTitleLabel, offset: 0)?.bottomParent(0)?.install()
+        
     }
     
     // 使用了 外部的一个变量来重写了这个 方法

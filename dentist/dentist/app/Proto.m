88ef2a7860d08a72e0289993be42121ba3785c74
fengzhenrong <345030038@qq.com>
2018-11-28 19:23:55 +0800

    job api

--- dentist/dentist/app/Proto.m
+++ dentist/dentist/app/Proto.m
@@ -1831,8 +1831,8 @@
 }
 
 
-#pragma mark Careers API
-//MARK:1.1.    查询所有职位列表
+#pragma mark --------------------------------------------Careers API
+//MARK:2.2.    查询所有职位列表
 + (void)queryAllJobs:(NSString *)sort categroy:(NSString *)categroy salary:(NSString *)salary experience:(NSString *)experience location:(NSString *)location distance:(NSString *)distance jobTitle:(NSString *)jobTitle company:(NSString *)company skip:(NSInteger)skip completed:(void(^)(NSArray<JobModel *> *array))completed {
     NSInteger limit=20;//分页数默认20条
     if (skip<=0) {
@@ -1888,9 +1888,115 @@
     }];
 }
 
-//MARK:1.1.    查询所有职位列表
+//MARK:2.2.    查询所有职位列表
 + (void)queryAllJobsNormal:(NSInteger)skip completed:(void(^)(NSArray<JobModel *> *array))completed {
     return [self queryAllJobs:nil categroy:nil salary:nil experience:nil location:nil distance:nil jobTitle:nil company:nil skip:skip completed:completed];
+}
+
+//MARK:2.7.   查询已申请职位列表
++ (void)queryAllApplicationJobs:(NSString *)sort categroy:(NSString *)categroy salary:(NSString *)salary experience:(NSString *)experience location:(NSString *)location distance:(NSString *)distance jobTitle:(NSString *)jobTitle company:(NSString *)company skip:(NSInteger)skip completed:(void(^)(NSArray<JobModel *> *array))completed {
+    NSInteger limit=20;//分页数默认20条
+    if (skip<=0) {
+        skip=0;
+    }
+    NSString *email=getLastAccount();
+    NSMutableDictionary *paradic=[NSMutableDictionary dictionary];
+    [paradic setObject:[NSNumber numberWithInteger:skip] forKey:@"skip"];
+    [paradic setObject:[NSNumber numberWithInteger:limit] forKey:@"limit"];
+    if (email) {
+        [paradic setObject:email forKey:@"email"];
+    }
+    if (sort) {
+        [paradic setObject:sort forKey:@"sort"];
+    }
+    if (categroy) {
+        [paradic setObject:categroy forKey:@"categroy"];
+    }
+    if (salary) {
+        [paradic setObject:salary forKey:@"salary"];
+    }
+    if (experience) {
+        [paradic setObject:experience forKey:@"experience"];
+    }
+    if (location) {
+        [paradic setObject:location forKey:@"location"];
+    }
+    if (distance) {
+        [paradic setObject:distance forKey:@"distance"];
+    }
+    if (jobTitle) {
+        [paradic setObject:jobTitle forKey:@"jobTitle"];
+    }
+    if (company) {
+        [paradic setObject:company forKey:@"company"];
+    }
+    
+    [self postAsync3:@"application/findAll" dic:paradic modular:@"hr" callback:^(HttpResult *r) {
+        if (r.OK) {
+            NSMutableArray *resultArray = [NSMutableArray array];
+            NSArray *arr = r.resultMap[@"data"];
+            for (NSDictionary *d in arr) {
+                JobModel *item = [[JobModel alloc] initWithJson:jsonBuild(d)];
+                if (item) {
+                    [resultArray addObject:item];
+                }
+            }
+            if (completed) {
+                completed(resultArray);
+            }
+        }else{
+            if (completed) {
+                completed(nil);
+            }
+        }
+    }];
+}
+
+//MARK:2.7.    查询所有职位列表
++ (void)queryAllApplicationJobs:(NSInteger)skip completed:(void(^)(NSArray<JobModel *> *array))completed {
+    return [self queryAllJobs:nil categroy:nil salary:nil experience:nil location:nil distance:nil jobTitle:nil company:nil skip:skip completed:completed];
+}
+
+//MARK:2.6.   添加职位申请接口
++(void)addJobApplication:(NSString *)jobId completed:(void(^)(HttpResult *result))completed
+{
+    NSMutableDictionary *paradic=[NSMutableDictionary dictionary];
+    if (jobId) {
+        [paradic setObject:jobId forKey:@"jobId"];
+    }
+    [self postAsync2:@"application/save" dic:paradic modular:@"hr"callback:^(HttpResult *r) {
+        if (completed) {
+            completed(r);
+        }
+    }];
+}
+
+//MARK:2.8.  添加职位关注接口
++(void)addJobBookmark:(NSString *)jobId completed:(void(^)(HttpResult *result))completed
+{
+    NSMutableDictionary *paradic=[NSMutableDictionary dictionary];
+    if (jobId) {
+        [paradic setObject:jobId forKey:@"jobId"];
+    }
+    [self postAsync2:@"bookmark/save" dic:paradic modular:@"hr"callback:^(HttpResult *r) {
+        if (completed) {
+            completed(r);
+        }
+    }];
+}
+
+//MARK:2.9.  删除职位关注接口
++(void)deleteJobBookmark:(NSString *)jobId completed:(void(^)(HttpResult *result))completed
+{
+    NSMutableDictionary *paradic=[NSMutableDictionary dictionary];
+    if (jobId) {
+        [paradic setObject:jobId forKey:@"jobId"];
+    }
+    [self postAsync2:@"bookmark/deleteOneById" dic:paradic modular:@"hr"callback:^(HttpResult *r) {
+        if (completed) {
+            completed(r);
+        }
+    }];
 }
 
 @end

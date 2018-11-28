a56ceacdd80e834ebda464e0e138679d0d25ee48
yangentao <yangentao@forsaven.com>
2018-11-28 22:25:05 +0800

    caree job detail

---
+++ dentist/dentist/careers/CareerJobDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  CareerJobDetailViewController.h
+//  dentist
+//
+//  Created by Shirley on 2018/11/28.
+//  Copyright © 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CareerJobDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ dentist/dentist/careers/View/JobDescriptionTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  JobDescriptionTableViewCell.m
+//  dentist
+//
+//  Created by Shirley on 2018/11/28.
+//  Copyright © 2018 thenextmediagroup.com. All rights reserved.
+//
+
+#import "JobDescriptionTableViewCell.h"
+
+@implementation JobDescriptionTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

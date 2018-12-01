76d2ca9e26e0d3903701dd9df0bb5ed923fcac7f
Su Ho V <hovansu.it@gmail.com>
2018-12-01 13:25:01 +0700

    unit test for cms bookmark

--- dentist/dentist/cms/CmsBookmarkController.h
+++ dentist/dentist/cms/CmsBookmarkController.h
@@ -5,7 +5,24 @@
 
 #import <UIKit/UIKit.h>
 #import "ListPage.h"
-
+#import "BookmarkModel.h"
+#import "HttpResult.h"
+#import "CMSModel.h"
 
 @interface CmsBookmarkController : ListPage
+@property NSString *categoryId;
+@property NSString *contentTypeId;
+@property CGFloat rowheight;
+
+-(void)updateFilterView;
+- (Class)viewClassOfItem:(NSObject *)item;
+- (CGFloat)heightOfItem:(NSObject *)item;
+- (void)onBindItem:(NSObject *)item view:(UIView *)view;
+- (void)BookMarkActionModel:(BookmarkModel *)model;
+- (void)handleDeleteBookmarkWithResult:(HttpResult *)result and:(BookmarkModel *)model;
+- (void)onClickItem:(NSObject *)item;
+- (void)clickFilter:(UIButton *)sender;
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView;
+- (void)handleSelectFilterWithCategory:(NSString *) category andType:(NSString *)type;
+- (void)handleQueryBookmarks:(NSArray<CMSModel *> *)array;
 @end

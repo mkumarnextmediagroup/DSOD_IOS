358c04acd35f2dc0001552350ad8fe9ab4aec5bf
wanglibo <>
2018-12-03 21:28:29 +0800

    job list of company detail

--- dentist/dentist/careers/View/CompanyOfJobDetailTableViewCell.m
+++ dentist/dentist/careers/View/CompanyOfJobDetailTableViewCell.m
@@ -9,11 +9,8 @@
 #import "CompanyOfJobDetailTableViewCell.h"
 #import "common.h"
 #import "CompanyModel.h"
-#import "FindJobsTableViewCell.h"
 
-@interface CompanyOfJobDetailTableViewCell()<UITableViewDelegate,UITableViewDataSource,JobsTableCellDelegate>
 
-@end
 @implementation CompanyOfJobDetailTableViewCell {
     UILabel *websiteValueLabel;
     UILabel *yearOfFoundationValueLabel;
@@ -23,8 +20,6 @@
     UILabel *detailValueLabel;
 
     int edge;
-    UITableView *myTable;
-    UILabel *jobCountTitle;
 }
 
 
@@ -32,85 +27,11 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         edge = 18;
-        [self buildTable];
+        [self buildView];
     }
     return self;
 }
 
--(void)buildTable
-{
-    myTable = [UITableView new];
-    [self.contentView addSubview:myTable];
-    myTable.dataSource = self;
-    myTable.delegate = self;
-    myTable.rowHeight = UITableViewAutomaticDimension;
-    myTable.estimatedRowHeight = 100;
-    [myTable setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-    myTable.tableHeaderView=[self makeHeaderView];
-    [myTable registerClass:[FindJobsTableViewCell class] forCellReuseIdentifier:NSStringFromClass([FindJobsTableViewCell class])];
-    
-    [[[myTable.layoutMaker sizeEq:SCREENWIDTH h:SCREENHEIGHT-NAVHEIGHT] topParent:0] install];
-}
-
--(void)setTotalCount:(NSInteger)totalCount{
-    _totalCount=totalCount;
-    if (_totalCount>0) {
-        NSString *jobcountstr=[NSString stringWithFormat:@"%@Jobs",@(_totalCount)];
-        NSString *jobstr=[NSString stringWithFormat:@"%@ | 5 New",jobcountstr];
-        NSMutableAttributedString *str = [[NSMutableAttributedString alloc] initWithString:jobstr];
-        [str addAttribute:NSForegroundColorAttributeName value:Colors.textMain range:NSMakeRange(0,jobcountstr.length+2)];
-        [str addAttribute:NSForegroundColorAttributeName value:Colors.textDisabled range:NSMakeRange(jobcountstr.length+2,jobstr.length - (jobcountstr.length+2))];
-        jobCountTitle.attributedText = str;
-    }else{
-        jobCountTitle.text=@"";
-    }
-}
-
--(void)setInfoArr:(NSMutableArray *)infoArr
-{
-    _infoArr=infoArr;
-    [self->myTable reloadData];
-}
-
-- (UIView *)makeHeaderView {
-    UIView *panel = [UIView new];
-    panel.frame = makeRect(0, 0, SCREENWIDTH, 32);
-    panel.backgroundColor=[UIColor clearColor];
-    
-    jobCountTitle=panel.addLabel;
-    jobCountTitle.font=[Fonts semiBold:13];
-    [[[[[jobCountTitle.layoutMaker leftParent:20] topParent:0] bottomParent:0] rightParent:40] install];
-    
-    UIButton *filterButton = [panel addButton];
-    [filterButton setImage:[UIImage imageNamed:@"desc"] forState:UIControlStateNormal];
-    [[[[filterButton.layoutMaker topParent:4] rightParent:-15] sizeEq:24 h:24] install];
-    [filterButton onClick:self action:@selector(clickFilter:)];
-    return panel;
-}
-
--(void)clickFilter:(UIButton *)sender
-{
-    
-}
-
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return _infoArr.count;
-}
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    FindJobsTableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:NSStringFromClass([FindJobsTableViewCell class]) forIndexPath:indexPath];
-    cell.delegate=self;
-    cell.indexPath=indexPath;
-    if (_infoArr && _infoArr.count>indexPath.row) {
-        cell.info=_infoArr[indexPath.row];
-    }
-    return cell;
-    
-    
-    
-}
 
 -(void)buildView{
     

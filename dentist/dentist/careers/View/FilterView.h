b5f6ee01b376fadb33bfd5f2c41a7913187dc803
sunxingguo <sunxingguo@forsaven.com>
2018-12-05 20:08:56 +0800

    update method name

--- dentist/dentist/careers/View/FilterView.h
+++ dentist/dentist/careers/View/FilterView.h
@@ -20,7 +20,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic,weak) id<FilterViewDelegate> delegate;
 
-+ (instancetype)initSliderView;
++ (instancetype)initFilterView;
 - (void)showFilter;
 @end
 

0b2d94098e419c0df65f6201bc7d9d4849a72a76
fengzhenrong <fengzhenrong@forsaven.com>
2018-12-12 19:11:27 +0800

    fix bugs

--- dentist/dentist/careers/CareerExplorePage.m
+++ dentist/dentist/careers/CareerExplorePage.m
@@ -105,7 +105,7 @@
         DSOProfilePage *dso = [DSOProfilePage new];
         UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:dso];
         navVC.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
-        [self presentViewController:navVC animated:YES completion:NULL];
+        [self presentViewController:navVC animated:NO completion:NULL];
 //        [self.navigationController pushViewController:dso animated:YES];
 
     }else if (btn.tag == 12)//review button click

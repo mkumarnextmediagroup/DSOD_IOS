dd5b468ccef8643b806f0c172bd3b616af2e2e26
fengzhenrong <fengzhenrong@forsaven.com>
2018-12-12 21:57:58 +0800

    uilabel top

---
+++ dentist/dentist/common/TopLeftLabel.h
@@ -0,0 +1,17 @@
+//
+//  TopLeftLabel.h
+//  dentist
+//
+//  Created by feng zhenrong on 2018/12/12.
+//  Copyright © 2018年 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TopLeftLabel : UILabel
+
+@end
+
+NS_ASSUME_NONNULL_END

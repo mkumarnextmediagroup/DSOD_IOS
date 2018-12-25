4b3bea37f8148d6365637a0425c48aaa19f59db3
Su Ho V <hovansu.it@gmail.com>
2018-12-25 22:40:57 +0700

    cms.view.playerview tests

--- dentist/dentist/cms/CMSDetail/CMSView/PlayerView.h
+++ dentist/dentist/cms/CMSDetail/CMSView/PlayerView.h
@@ -19,9 +19,15 @@
 @property (nonatomic,strong)UIButton *greeBtn;
 @property (nonatomic,strong)UIButton *moreButton;
 @property (nonatomic,strong)UIButton *markButton;
+@property BOOL authorDSODentist;
+@property UIWebView *mywebView;
 
-
--(void)bind:(DetailModel *)bindInfo ;
+- (void)bind:(DetailModel *)bindInfo;
 - (void)resetLayout;
+- (NSString *)htmlString:(NSString *)html;
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation;
+- (void)webViewDidFinishLoad:(UIWebView *)webView;
+- (void) handleCompletion:(WKWebView *)webView result:(id _Nullable) result;
+- (void) handleCompletionJavaScript:(id _Nullable) result ratio: (CGFloat) ratio;
 
 @end

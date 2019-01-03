9c84122528a2efbb2ebde8999245442075a67397
Jacksun <>
2019-01-03 14:48:53 +0800

    add the close button

--- dentist/dentist/careers/jobDetail/UploadResumeView.m
+++ dentist/dentist/careers/jobDetail/UploadResumeView.m
@@ -75,11 +75,22 @@ static dispatch_once_t onceToken;
     }
 }
 
+- (void)closeBtnClick
+{
+    [UploadResumeView hide];
+}
+
 - (void)initUploadView
 {
     uploadView = [instance addView];
     uploadView.backgroundColor = [UIColor whiteColor];
     [[[[uploadView.layoutMaker leftParent:edge] sizeEq:SCREENWIDTH-edge*2 h:320] centerYParent:0] install];
+    
+    
+    UIButton *closeBtn = [uploadView addButton];
+    [closeBtn addTarget:self action:@selector(closeBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [closeBtn setImage:[UIImage imageNamed:@"close"] forState:UIControlStateNormal];
+    [[[[closeBtn.layoutMaker sizeEq:36 h:36] rightParent:-16] topParent:10] install];
     
     UITapGestureRecognizer *singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(sigleTappedPickerView:)];
     [singleTap setNumberOfTapsRequired:1];

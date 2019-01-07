091bfc03b8970b4262a3dc4c6b9c604ec659ebba
yangentao <yangentao@forsaven.com>
2019-01-07 11:09:56 +0800

    empty view controller of sprint 8

---
+++ dentist/dentist/Settings/HelpAndFeedbackViewController.m
@@ -0,0 +1,32 @@
+//
+//  HelpAndFeedbackViewController.m
+//  dentist
+//
+//  Created by Shirley on 2019/1/6.
+//  Copyright © 2019 thenextmediagroup.com. All rights reserved.
+//
+
+#import "HelpAndFeedbackViewController.h"
+
+@interface HelpAndFeedbackViewController ()
+
+@end
+
+@implementation HelpAndFeedbackViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ dentist/dentist/Settings/VideoQualityViewController.m
@@ -0,0 +1,19 @@
+//
+//  VideoQualityViewController.m
+//  dentist
+//
+//  Created by Shirley on 2019/1/5.
+//  Copyright © 2019 thenextmediagroup.com. All rights reserved.
+//
+
+#import "VideoQualityViewController.h"
+#import "Common.h"
+#import "SettingLabelAndCheckedTableViewCell.h"
+
+@interface VideoQualityViewController ()
+
+@end
+
+
+@implementation VideoQualityViewController
+@end

368966257adc205e10de7e21db840720e061c2f5
fengzhenrong <345030038@qq.com>
2019-01-23 19:06:15 +0800

    fix bugs

--- dentist/dentist/careers/CareerAlertsViewController.m
+++ dentist/dentist/careers/CareerAlertsViewController.m
@@ -23,6 +23,7 @@
     NSIndexPath *editingIndexPath;
     UIRefreshControl *refreshControl;
     BOOL isdownrefresh;
+    BOOL isfirstfresh;
 }
 @end
 
@@ -100,6 +101,7 @@
     [self showIndicator];
     [Proto queryRemindsByUserId:0 completed:^(NSArray<JobAlertsModel *> *array, NSInteger totalCount) {
         foreTask(^{
+            self->isfirstfresh=YES;
             [self hideIndicator];
             NSLog(@"%@",array);
             self->infoArr = [NSMutableArray arrayWithArray:array];
@@ -133,53 +135,56 @@
         [self->myTable setScrollEnabled:NO];
         UIView *headerVi = [UIView new];
         [sender addSubview:headerVi];
-        [[[headerVi.layoutMaker sizeEq:SCREENWIDTH h:SCREENHEIGHT-NAVHEIGHT-TABLEBAR_HEIGHT] topParent:0] install];
-        headerVi.backgroundColor = [UIColor clearColor];
-        UIButton *headBtn = headerVi.addButton;
-        [headBtn setImage:[UIImage imageNamed:@"noun_mobile notification"] forState:UIControlStateNormal];
-        headBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
-        headBtn.titleLabel.font = [Fonts regular:13];
-        [[[headBtn.layoutMaker centerXParent:0] centerYParent:-80] install];
-        UILabel *tipLabel= headerVi.addLabel;
-        tipLabel.textAlignment=NSTextAlignmentCenter;
-        tipLabel.numberOfLines=0;
-        tipLabel.font = [Fonts semiBold:16];
-        tipLabel.textColor =[UIColor blackColor];
-        tipLabel.text=@"Add your first Job Alert!";
-        [[[[tipLabel.layoutMaker leftParent:10] rightParent:-10] below:headBtn offset:50] install];
+        if (self->isfirstfresh) {
+            [[[headerVi.layoutMaker sizeEq:SCREENWIDTH h:SCREENHEIGHT-NAVHEIGHT-TABLEBAR_HEIGHT] topParent:0] install];
+            headerVi.backgroundColor = [UIColor clearColor];
+            UIButton *headBtn = headerVi.addButton;
+            [headBtn setImage:[UIImage imageNamed:@"noun_mobile notification"] forState:UIControlStateNormal];
+            headBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+            headBtn.titleLabel.font = [Fonts regular:13];
+            [[[headBtn.layoutMaker centerXParent:0] centerYParent:-80] install];
+            UILabel *tipLabel= headerVi.addLabel;
+            tipLabel.textAlignment=NSTextAlignmentCenter;
+            tipLabel.numberOfLines=0;
+            tipLabel.font = [Fonts semiBold:16];
+            tipLabel.textColor =[UIColor blackColor];
+            tipLabel.text=@"Add your first Job Alert!";
+            [[[[tipLabel.layoutMaker leftParent:10] rightParent:-10] below:headBtn offset:50] install];
+            
+            UILabel *tipLabel2= headerVi.addLabel;
+            tipLabel2.textAlignment=NSTextAlignmentCenter;
+            tipLabel2.numberOfLines=0;
+            tipLabel2.font = [UIFont systemFontOfSize:15.0];
+            tipLabel2.textColor =[UIColor blackColor];
+            tipLabel2.text=@"Receive regular emails with the best matched jobs \nfrom DSOs";
+            [[[[tipLabel2.layoutMaker leftParent:10] rightParent:-10] below:tipLabel offset:10] install];
+            
+            
+            UILabel *addlabel=[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
+            addlabel.font=[UIFont systemFontOfSize:20];
+            addlabel.text=@"+";//[Fonts semiBold:30]
+            addlabel.textColor=[UIColor whiteColor];
+            addlabel.textAlignment=NSTextAlignmentCenter;
+            addlabel.layer.cornerRadius = 2;
+            addlabel.layer.masksToBounds = YES;
+            
+            //设置边框及边框颜色
+            addlabel.layer.borderWidth = 1;
+            
+            addlabel.layer.borderColor =[UIColor whiteColor].CGColor;
+            UIImage *addimage=[UIImage getmakeImageWithView:addlabel];
+            
+            UIButton *createview=headerVi.addButton;
+            [createview setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            createview.titleLabel.font=[UIFont systemFontOfSize:15.0];
+            [createview setTitle:@"Create Job Alert" forState:UIControlStateNormal];
+            [createview setImage:addimage forState:UIControlStateNormal];
+            createview.backgroundColor=Colors.textDisabled;
+            createview.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0);
+            [createview addTarget:self action:@selector(addClick) forControlEvents:UIControlEventTouchUpInside];
+            [[[[[createview.layoutMaker leftParent:20] rightParent:-20] bottomParent:-30]  heightEq:44] install];
+        }
         
-        UILabel *tipLabel2= headerVi.addLabel;
-        tipLabel2.textAlignment=NSTextAlignmentCenter;
-        tipLabel2.numberOfLines=0;
-        tipLabel2.font = [UIFont systemFontOfSize:15.0];
-        tipLabel2.textColor =[UIColor blackColor];
-        tipLabel2.text=@"Receive regular emails with the best matched jobs \nfrom DSOs";
-        [[[[tipLabel2.layoutMaker leftParent:10] rightParent:-10] below:tipLabel offset:10] install];
-        
-        
-        UILabel *addlabel=[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
-        addlabel.font=[UIFont systemFontOfSize:20];
-        addlabel.text=@"+";//[Fonts semiBold:30]
-        addlabel.textColor=[UIColor whiteColor];
-        addlabel.textAlignment=NSTextAlignmentCenter;
-        addlabel.layer.cornerRadius = 2;
-        addlabel.layer.masksToBounds = YES;
-        
-        //设置边框及边框颜色
-        addlabel.layer.borderWidth = 1;
-        
-        addlabel.layer.borderColor =[UIColor whiteColor].CGColor;
-        UIImage *addimage=[UIImage getmakeImageWithView:addlabel];
-        
-        UIButton *createview=headerVi.addButton;
-        [createview setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        createview.titleLabel.font=[UIFont systemFontOfSize:15.0];
-        [createview setTitle:@"Create Job Alert" forState:UIControlStateNormal];
-        [createview setImage:addimage forState:UIControlStateNormal];
-        createview.backgroundColor=Colors.textDisabled;
-        createview.titleEdgeInsets = UIEdgeInsetsMake(0, 20, 0, 0);
-        [createview addTarget:self action:@selector(addClick) forControlEvents:UIControlEventTouchUpInside];
-        [[[[[createview.layoutMaker leftParent:20] rightParent:-20] bottomParent:-30]  heightEq:44] install];
         return headerVi;
     } normalBlock:^(UITableView * _Nonnull sender) {
         [self->myTable setScrollEnabled:YES];

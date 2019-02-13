2614685f5241e52bfee6e11da27d84543d97155e
yangentao <yangentao@forsaven.com>
2019-02-13 12:21:35 +0800

    LMS speaker detail

---
+++ dentist/dentist/education/EducationSpeakerDetailViewController.m
@@ -0,0 +1,22 @@
+//
+//  EducationSpeakerDetailViewController.m
+//  dentist
+//
+//  Created by Shirley on 2019/2/13.
+//  Copyright © 2019 thenextmediagroup.com. All rights reserved.
+//
+
+#import "EducationSpeakerDetailViewController.h"
+
+@interface EducationSpeakerDetailViewController ()
+
+@end
+
+@implementation EducationSpeakerDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end

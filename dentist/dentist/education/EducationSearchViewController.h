e724078615d8cc3a9a1df3239b0642c5a697e998
fengzhenrong <fengzhenrong@forsaven.com>
2019-02-13 19:09:14 +0800

    fix bugs

---
+++ dentist/dentist/education/EducationSearchViewController.h
@@ -0,0 +1,17 @@
+//
+//  EducationSearchViewController.h
+//  dentist
+//
+//  Created by feng zhenrong on 2019/2/13.
+//  Copyright © 2019年 thenextmediagroup.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EducationSearchViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

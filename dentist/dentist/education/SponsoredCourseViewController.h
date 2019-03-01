b63f88022278cf97a80181eadc66f521c3b6cbb8
Su Ho V <hovansu.it@gmail.com>
2019-03-01 20:09:23 +0700

    education tests

--- dentist/dentist/education/SponsoredCourseViewController.h
+++ dentist/dentist/education/SponsoredCourseViewController.h
@@ -12,6 +12,20 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface SponsoredCourseViewController : UIViewController
 @property (nonatomic,strong) NSString *sponsorId;
+
+- (void)viewDidLoad;
+- (UIView *)makeHeaderView;
+- (void)searchClick;
+- (void)back;
+- (void)refreshData;
+- (void)setupRefresh;
+- (void)refreshClick:(UIRefreshControl *)refreshControl;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
 @end
 
 NS_ASSUME_NONNULL_END
---
+++ dentist/dentistTests/Module/Education/SponsoredCourseViewControllerTests.m
@@ -0,0 +1,36 @@
+//
+//  SponsoredCourseViewControllerTests.m
+//  dentistTests
+//
+//  Created by Su Ho V. on 3/1/19.
+//  Copyright © 2019 thenextmediagroup.com. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "SponsoredCourseViewController.h"
+
+SPEC_BEGIN(SponsoredCourseViewControllerTests)
+describe(@"Unit Test For SponsoredCourseViewController", ^{
+    __block SponsoredCourseViewController *controller;
+
+    beforeEach(^{
+        controller = [SponsoredCourseViewController new];
+    });
+
+    context(@"methods", ^{
+//        - (void)viewDidLoad;
+//        - (UIView *)makeHeaderView;
+//        - (void)searchClick;
+//        - (void)back;
+//        - (void)refreshData;
+//        - (void)setupRefresh;
+//        - (void)refreshClick:(UIRefreshControl *)refreshControl;
+//        - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
+//        - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
+//        - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
+//        - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+//        - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+//        - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+    });
+});
+SPEC_END

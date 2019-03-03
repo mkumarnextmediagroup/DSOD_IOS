1727b155f867afa131a24b138b3b4e9526172dcc
Su Ho V <hovansu.it@gmail.com>
2019-03-03 13:43:59 +0700

    education download tests

--- dentist/dentistTests/Module/Education/EducationCategoryCourseViewControllerTests.m
+++ dentist/dentistTests/Module/Education/EducationCategoryCourseViewControllerTests.m
@@ -8,6 +8,7 @@
 
 #import "Kiwi.h"
 #import "EducationCategoryCourseViewController.h"
+#import "CourseTableViewCell.h"
 
 SPEC_BEGIN(EducationCategoryCourseViewControllerTests)
 describe(@"Unit Test For EducationCategoryCourseViewController", ^{
@@ -48,12 +49,37 @@ describe(@"Unit Test For EducationCategoryCourseViewController", ^{
             [[theValue(controller.view) shouldNot] beNil];
         });
 
-//        - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
-//        - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
-//        - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
-//        - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
-//        - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-//        - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+        it(@"numberOfSectionsInTableView", ^{
+            NSInteger num = [controller numberOfSectionsInTableView:[UITableView new]];
+            [[theValue(num) should] equal: theValue(1)];
+        });
+
+        it(@"heightForHeaderInSection", ^{
+            CGFloat h = [controller tableView:[UITableView new] heightForHeaderInSection:0];
+            [[theValue(h) should] equal: theValue(15)];
+        });
+
+        it(@"viewForHeaderInSection", ^{
+            UIView *view = [controller tableView:[UITableView new] viewForHeaderInSection:0];
+            [[theValue(view) shouldNot] beNil];
+        });
+
+        it(@"numberOfRowsInSection", ^{
+            NSInteger num = [controller tableView:[UITableView new] numberOfRowsInSection:0];
+            [[theValue(num) should] equal:theValue(0)];
+        });
+
+        it(@"cellForRowAtIndexPath", ^{
+            UITableView *tb = [UITableView new];
+            [tb registerClass:[CourseTableViewCell class] forCellReuseIdentifier:NSStringFromClass([CourseTableViewCell class])];
+            UITableViewCell *cell = [controller tableView:tb cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+            [[theValue(cell) shouldNot] beNil];
+        });
+
+        it(@"didSelectRowAtIndexPath", ^{
+            [controller tableView:[UITableView new] didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
     });
 });
 SPEC_END
--- dentist/dentistTests/Module/Education/SponsoredCourseViewControllerTests.m
+++ dentist/dentistTests/Module/Education/SponsoredCourseViewControllerTests.m
@@ -8,6 +8,7 @@
 
 #import "Kiwi.h"
 #import "SponsoredCourseViewController.h"
+#import "CourseTableViewCell.h"
 
 SPEC_BEGIN(SponsoredCourseViewControllerTests)
 describe(@"Unit Test For SponsoredCourseViewController", ^{
@@ -18,19 +19,72 @@ describe(@"Unit Test For SponsoredCourseViewController", ^{
     });
 
     context(@"methods", ^{
-//        - (void)viewDidLoad;
-//        - (UIView *)makeHeaderView;
-//        - (void)searchClick;
-//        - (void)back;
-//        - (void)refreshData;
-//        - (void)setupRefresh;
-//        - (void)refreshClick:(UIRefreshControl *)refreshControl;
-//        - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
-//        - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
-//        - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
-//        - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
-//        - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
-//        - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+        it(@"viewDidLoad", ^{
+            [controller viewDidLoad];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
+
+        it(@"makeHeaderView", ^{
+            UIView *v = [controller makeHeaderView];
+            [[theValue(v) shouldNot] beNil];
+        });
+
+        it(@"searchClick", ^{
+            [controller searchClick];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
+
+        it(@"back", ^{
+            [controller back];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
+
+        it(@"refreshData", ^{
+            [controller refreshData];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
+
+        it(@"setupRefresh", ^{
+            [controller setupRefresh];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
+
+        it(@"refreshClick", ^{
+            [controller refreshClick:[UIRefreshControl new]];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
+
+        it(@"numberOfSectionsInTableView", ^{
+            NSInteger num = [controller numberOfSectionsInTableView:[UITableView new]];
+            [[theValue(num) should] equal: theValue(1)];
+        });
+
+        it(@"heightForHeaderInSection", ^{
+            CGFloat h = [controller tableView:[UITableView new] heightForHeaderInSection:0];
+            [[theValue(h) should] equal: theValue(50)];
+        });
+
+        it(@"viewForHeaderInSection", ^{
+            UIView *view = [controller tableView:[UITableView new] viewForHeaderInSection:0];
+            [[theValue(view) shouldNot] beNil];
+        });
+
+        it(@"numberOfRowsInSection", ^{
+            NSInteger num = [controller tableView:[UITableView new] numberOfRowsInSection:0];
+            [[theValue(num) should] equal:theValue(0)];
+        });
+
+        it(@"cellForRowAtIndexPath", ^{
+            UITableView *tb = [UITableView new];
+            [tb registerClass:[CourseTableViewCell class] forCellReuseIdentifier:NSStringFromClass([CourseTableViewCell class])];
+            UITableViewCell *cell = [controller tableView:tb cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+            [[theValue(cell) shouldNot] beNil];
+        });
+
+        it(@"didSelectRowAtIndexPath", ^{
+            [controller tableView:[UITableView new] didSelectRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+            [[theValue(controller.view) shouldNot] beNil];
+        });
     });
 });
 SPEC_END

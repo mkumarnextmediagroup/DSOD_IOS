c01977819f78d231216938f81aa8ca4d0d2d71b6
Su Ho V <hovansu.it@gmail.com>
2019-03-09 13:07:52 +0700

    add test

--- dentist/dentist/education/downloadtest/DentistHomeVC.h
+++ dentist/dentist/education/downloadtest/DentistHomeVC.h
@@ -11,4 +11,17 @@
 
 @interface DentistHomeVC : UIViewController
 
+- (NSMutableArray<DentistDownloadModel *> *)dataSource;
+- (void)viewDidLoad;
+- (void)back;
+- (void)viewWillAppear:(BOOL)animated;
+- (void)getInfo;
+- (void)getCacheData;
+- (void)creatControl;
+- (void)addNotification;
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+- (void)downLoadProgress:(NSNotification *)notification;
+- (void)downLoadStateChange:(NSNotification *)notification;
 @end
---
+++ dentist/dentistTests/Module/Education/DownloadTest/DentistHomeVCTests.m
@@ -0,0 +1,37 @@
+//
+//  DentistHomeVCTests.m
+//  dentistTests
+//
+//  Created by Su Ho V. on 3/4/19.
+//  Copyright © 2019 thenextmediagroup.com. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "DentistHomeVC.h"
+
+SPEC_BEGIN(DentistHomeVCTests)
+describe(@"Unit Test For DentistHomeVC", ^{
+    __block DentistHomeVC *controller;
+
+    beforeEach(^{
+        controller = [DentistHomeVC new];
+    });
+
+    context(@"methods", ^{
+//        - (NSMutableArray<DentistDownloadModel *> *)dataSource;
+//        - (void)viewDidLoad;
+//        - (void)back;
+//        - (void)viewWillAppear:(BOOL)animated;
+//        - (void)getInfo;
+//        - (void)getCacheData;
+//        - (void)creatControl;
+//        - (void)addNotification;
+//        - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+//        - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+//        - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+//        - (void)downLoadProgress:(NSNotification *)notification;
+//        - (void)downLoadStateChange:(NSNotification *)notification;
+
+    });
+});
+SPEC_END
